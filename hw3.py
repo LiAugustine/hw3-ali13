f784ded2f1b1de5931fefac9452921b73650618b
LiAugustine <77209073+LiAugustine@users.noreply.github.com>
2022-01-24 21:06:57 -0500

    Question 2 Function
    Added question 2 function

--- hw3.py
+++ hw3.py
@@ -1,4 +1,5 @@
-##CSC 4350 HW#3 - AugustineLi
+import math
+##CSC 4350 HW #3 - AugustineLi
 
 ##Question 1 function
 def question_one(str):
@@ -23,6 +24,7 @@ def question_one(str):
   return None ##equal vowels and consonants
   
 ##Test cases
+print('Question 1 Test Cases:')
 print(question_one("area")) ##should return true
 print(question_one("iota")) ##should return true
 
@@ -31,3 +33,13 @@ print(question_one("hello")) ##should return false
 
 print(question_one("ttee")) ##should return none
 print(question_one("aaff")) ##should return none
+print()
+
+##Question 2 Function
+def question_two(R, H): ##two inputs, radius and height
+  cylinderVolume = math.pi * H * R * R ##math.pi = pi value
+  return 'The volume of a cylider with radius ' + str(R) + ' and height ' + str(H) + ' is ' + str(cylinderVolume) ##clear return statement. must use str() to convert number into string to properly concatenate
+
+print('Question 2 Test Cases:')
+print(question_two(2, 2)) ##should be 25.13
+print(question_two(3, 4)) ##should be 113.1
