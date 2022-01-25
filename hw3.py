73ec259801ac6b8fab6b9bf60c16fbe539c34124
LiAugustine <77209073+LiAugustine@users.noreply.github.com>
2022-01-24 20:47:40 -0500

    Question one function

--- hw3.py
+++ hw3.py
@@ -1,1 +1,33 @@
+##CSC 4350 HW#3 - AugustineLi
 
+##Question 1 function
+def question_one(str):
+  
+  vowelCount = 0
+  consonantCount = 0
+
+  vowels = set("aeiouAEIOU") ##set which contains vowels, lowercase and uppercase
+
+  for alphabet in str:
+    if alphabet in vowels: ##if letter in str is a vowel
+      vowelCount = vowelCount + 1 ##increment vowel count
+    else: ##if not a vowel, has to be a consonant
+      consonantCount = consonantCount + 1 
+
+
+  if (vowelCount > consonantCount): ##more vowels than consonants
+    return True
+  elif (consonantCount > vowelCount): ##more consonants than vowels
+    return False
+
+  return None ##equal vowels and consonants
+  
+##Test cases
+print(question_one("area")) ##should return true
+print(question_one("iota")) ##should return true
+
+print(question_one("test")) ##should return false
+print(question_one("hello")) ##should return false
+
+print(question_one("ttee")) ##should return none
+print(question_one("aaff")) ##should return none
